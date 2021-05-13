6e6c02c4ff0fca036c6d21458f0d62a4c0fd52eb
Sameer Jagtap <jagtap.sameer00@gmail.com>
2021-05-13 08:57:29 +0530

    Added VIPER Module with Protocols and Entity

---
+++ VIPER/VIPER/Modules/Quotes/QuotesViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class QuotesViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
